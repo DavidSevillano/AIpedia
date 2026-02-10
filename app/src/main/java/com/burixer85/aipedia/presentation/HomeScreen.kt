20c4f1a995fadb6280620dc847f1f1ed37a91193
Burixer85 <dvd.sevillano@gmail.com>
2026-02-10 19:29:50 +0100

    feat(navigation): Add NavigationHost and define initial routes for HomeScreen

---
+++ app/src/main/java/com/burixer85/aipedia/presentation/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.burixer85.aipedia.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen(viewmodel: HomeViewModel = hiltViewModel()) {
+    
+}
