e3ae487357f12a3698e34c1c9e2323c4ad4df5a6
Burixer85 <dvd.sevillano@gmail.com>
2026-02-14 19:24:22 +0100

    feat: Implement Room DB with many-to-many AI-Category relationship

--- app/src/main/java/com/burixer85/aipedia/data/local/AipediaDatabase.kt
+++ app/src/main/java/com/burixer85/aipedia/data/local/AipediaDatabase.kt
@@ -3,6 +3,7 @@ package com.burixer85.aipedia.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.burixer85.aipedia.data.local.dao.AiDao
+import com.burixer85.aipedia.data.local.dao.CategoryDao
 import com.burixer85.aipedia.data.local.entities.AiCategoryCrossRef
 import com.burixer85.aipedia.data.local.entities.AiEntity
 import com.burixer85.aipedia.data.local.entities.CategoryEntity
@@ -12,7 +13,7 @@ import com.burixer85.aipedia.data.local.entities.CategoryEntity
     version = 1,
     exportSchema = false
 )
-
-abstract class AiDatabase : RoomDatabase() {
+abstract class AipediaDatabase : RoomDatabase() {
     abstract fun aiDao(): AiDao
+    abstract fun categoryDao(): CategoryDao
 }
